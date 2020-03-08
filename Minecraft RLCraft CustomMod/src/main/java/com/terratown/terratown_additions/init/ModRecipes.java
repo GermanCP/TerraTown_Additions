35ca967afe5d4b9fbd3d3198cf888041abfdea56
GermanCreepPlay <creepplay1@gmail.com>
2020-03-08 15:03:54 +0100

    Upload V1.0
    Upload of Version 1.0 to CurseForge
    Review Pending
    all basic functions are implemented and working

---
+++ Minecraft RLCraft CustomMod/src/main/java/com/terratown/terratown_additions/init/ModRecipes.java
@@ -0,0 +1,11 @@
+package com.terratown.terratown_additions.init;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModRecipes {
+	
+	public static void init()
+	{
+
+	}
+}
